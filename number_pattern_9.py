467c6df938f6e08acde72471cf5b41bb0d7c231d
sarthakankolekar-lab <sarthakankolekar@gmail.com>
2026-07-05 18:47:37 +0530

    number_pattern_9

--- number_pattern_9.py
+++ number_pattern_9.py
@@ -1,6 +1,18 @@
+#Code
 n = int(input())
 
 for i in range(n , 0 , -1):
     for j in range(i , 0 , -1):
         print(j,end=" ")
     print()
+
+#Output
+"""
+5
+
+5 4 3 2 1 
+4 3 2 1 
+3 2 1 
+2 1 
+1 
+"""

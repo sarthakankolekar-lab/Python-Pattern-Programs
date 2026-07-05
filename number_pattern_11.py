a64370feb81f1ff54511a598bc85adec98c836bd
sarthakankolekar-lab <sarthakankolekar@gmail.com>
2026-07-05 14:47:02 +0530

    number_pattern_11

--- number_pattern_11.py
+++ number_pattern_11.py
@@ -1,7 +1,7 @@
 n = int(input())
 
 for i in range(1 , n + 1):
-    print(" "*((n*2)-(i*2)),end="")
+    print(" "*( (n * 2) - (i * 2) ),end="")
     for j in range(1 , i + 1):
         print(j,end=" ")
     print()

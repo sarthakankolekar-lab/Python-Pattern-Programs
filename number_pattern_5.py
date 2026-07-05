6ead340a45f0db06bbfd021f4d06ed8c5d5cbb13
sarthakankolekar-lab <sarthakankolekar@gmail.com>
2026-07-05 18:35:36 +0530

    number_pattern_5

--- number_pattern_5.py
+++ number_pattern_5.py
@@ -1,7 +1,18 @@
+#Code
 n = int(input())
 
 for i in range(n , 0 , -1):
     for j in range(i):
         print(j , end=" ")
     print()
-    
+
+#Output
+"""
+5
+
+0 1 2 3 4 
+0 1 2 3 
+0 1 2 
+0 1 
+0 
+"""

1177a187399f469cfa9fa370f938215330c45b69
sarthakankolekar-lab <sarthakankolekar@gmail.com>
2026-07-06 07:19:23 +0530

    number_pattern_12

--- number_pattern_12.py
+++ number_pattern_12.py
@@ -1,3 +1,4 @@
+#Code
 n = int(input())
 
 for i in range(1, n + 1):
@@ -7,3 +8,15 @@ for i in range(1, n + 1):
         else:
             print(j, end=" ")
     print()
+
+#Output
+"""
+6
+
+1 2 3 4 5 6 
+2 2 3 4 5 6 
+3 3 3 4 5 6 
+4 4 4 4 5 6 
+5 5 5 5 5 6 
+6 6 6 6 6 6 
+"""
